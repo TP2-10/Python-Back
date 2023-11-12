6064de4e5fd4d424de9815886afa0c71c9a9bacb
Erick.BH <erickalonsobh@gmail.com>
2023-11-12 13:59:16 -0500

    Added Deployments files
    https://tp2-backend.rj.r.appspot.com/
    
    deployed url

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 SECRET_KEY = 'your_secret_key'
-SQLALCHEMY_DATABASE_URI = 'mysql://root:1np#4Uzs@localhost/pythonback'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:1np#4Uzs@34.122.228.36/pythonback'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
